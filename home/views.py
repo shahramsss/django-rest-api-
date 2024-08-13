2b2d4f0169b9b5a9d703ce62e375361b7f2d9079
sss <shahramsss1987@gmail.com>
2024-08-13 11:57:32 +0330

    add home functions and class base view

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+@api_view(["GET", "POST", "PUT"])
+def home(request):
+    return Response({"name": "sss"})
+
+
+class Home(APIView):
+    def get(self, request):
+        return Response(data={"name": "sss class base view"})
--- restapi/settings.py
+++ restapi/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # local apps
-    "rest"
+    "rest",
+    "home",
     # third apps
     "rest_framework",
 ]
