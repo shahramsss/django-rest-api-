027df148323ff6fa60cfc8ef500f698b23b7dca5
sss <shahramsss1987@gmail.com>
2024-08-15 10:26:03 +0330

    add user register rest

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import UserRegistrSerialiser
+
+
+class UserRegister(APIView):
+    def post(self, request):
+        ser_data = UserRegistrSerialiser(data=request.POST)
+        if ser_data.is_valid():
+            User.objects.create(
+                username=ser_data.validated_data["username"],
+                email=ser_data.validated_data["email"],
+                password=ser_data.validated_data["password"],
+            )
+            return Response(data=ser_data.data)
+        return Response(ser_data.errors)
--- restapi/settings.py
+++ restapi/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     # local apps
     "rest",
     "home",
+    "accounts",
+
     # third apps
     "rest_framework",
 ]
