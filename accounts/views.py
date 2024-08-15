0562677228bd65a14e1f7dccbf925ab34c221784
sss <shahramsss1987@gmail.com>
2024-08-15 11:38:26 +0330

    add status code in create user

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserRegistrSerialiser , UserRegistrModelSerialiser
+from rest_framework import status 
 
 
 class UserRegister(APIView):
@@ -19,5 +20,5 @@ class UserRegister(APIView):
             # )
 
             ser_data.create(ser_data.validated_data)
-            return Response(data=ser_data.data)
-        return Response(ser_data.errors)
+            return Response(data=ser_data.data , status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors , status=status.HTTP_400_BAD_REQUEST)
