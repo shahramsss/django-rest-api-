0e2635860769df60107683186a8041bc75039cec
sss <shahramsss1987@gmail.com>
2024-08-15 12:08:28 +0330

    add auth token

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as auth_token
 
 app_name = "accounts"
 
 urlpatterns = [
     path("register/", views.UserRegister.as_view(), name="register"),
+    path("api-token-auth/", auth_token.obtain_auth_token),
 ]
--- restapi/settings.py
+++ restapi/settings.py
@@ -37,12 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "rest_framework.authtoken",
     # local apps
     "rest",
     "home",
     "accounts",
-
     # third apps
     "rest_framework",
 ]
@@ -129,3 +128,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
