cd4cb7691213c05fc1a7d81be3ed85b9d2ac9543
sss <shahramsss1987@gmail.com>
2024-08-20 10:21:46 +0330

    add viewsets and routers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -48,3 +48,9 @@ class UserRegistrModelSerialiser(serializers.ModelSerializer):
         if data["password"] != data["password2"]:
             raise serializers.ValidationError("passwords not match")
         return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .serializers import UserRegistrSerialiser , UserRegistrModelSerialiser
-from rest_framework import status 
+from .serializers import (
+    UserRegistrSerialiser,
+    UserRegistrModelSerialiser,
+    UserSerializer,
+)
+from rest_framework import status, viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 
 class UserRegister(APIView):
@@ -20,5 +26,42 @@ class UserRegister(APIView):
             # )
 
             ser_data.create(ser_data.validated_data)
-            return Response(data=ser_data.data , status=status.HTTP_201_CREATED)
-        return Response(ser_data.errors , status=status.HTTP_400_BAD_REQUEST)
+            return Response(data=ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ViewSet):
+    permission_classes = [
+        IsAuthenticated,
+    ]
+    queryset = User.objects.all()
+
+    def list(self, request):
+        ser_data = UserSerializer(instance=self.queryset, many=True)
+        return Response(data=ser_data.data, status=status.HTTP_200_OK)
+
+    def update(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        ser_data = UserSerializer(instance=user)
+        return Response(data=ser_data.data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        if user != request.user:
+            return Response(data={"permission denied": "your are not owner"})
+        ser_data = UserSerializer(instance=user, data=request.POST, partial=True)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(data=ser_data.data)
+        return Response(data=ser_data.errors)
+
+    def destroy(self, request, pk=None):
+        user = get_object_or_404(User, pk=pk)
+        if user != request.user:
+            return Response(data={"permission denied": "your are not owner"})
+        user.is_active = False
+        user.save()
+        return Response(data={"message": "user deactivcted"})
